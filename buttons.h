8e3451c9a6e706f61b5d74fd49c6ece8fd29f29f
MagdaRyb <magdalena.marta.rybicka@gmail.com>
2017-01-23 17:09:56 +0100

    Update buttons.h

--- buttons.h
+++ buttons.h
@@ -2,6 +2,11 @@
 #define buttons_h
 
 #include "MKL46Z4.h"     
+#define SW1_PIN 3												
+#define SW3_PIN 12
+#define Button_1 10
+#define Button_2 11
+#define PORTC_D_IRQ_NBR (IRQn_Type) 31				
 
 void buttonsInitialize(void);
 
